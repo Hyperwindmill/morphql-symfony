0ed8c08d58d8210eb739eddb99699536d03866cb
Hyperwindmill <48651572+Hyperwindmill@users.noreply.github.com>
2026-02-14 23:41:18 +0100

    Add getContainerExtension method to MorphQLBundle

--- src/MorphQLBundle.php
+++ src/MorphQLBundle.php
@@ -1,9 +1,18 @@
 <?php
-
 namespace MorphQL\SymfonyBundle;
 
+use MorphQL\SymfonyBundle\DependencyInjection\MorphQLExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MorphQLBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        if (null === $this->extension) {
+            $this->extension = new MorphQLExtension();
+        }
+
+        return $this->extension;
+    }
 }
